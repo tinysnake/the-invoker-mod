b37cc7ab223467c1d462fd96336169cc18b8059d
SNAKE-OFFICE <tff.assist@gmail.com>
2013-06-06 18:49:11 +0800

    do some gui psd and writing the ContainerSoulSmelter.java

---
+++ common/snake/mcmods/theinvoker/inventory/ContainerSoulSmelter.java
@@ -0,0 +1,81 @@
+package snake.mcmods.theinvoker.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import snake.mcmods.theinvoker.tileentities.TileSoulSmelter;
+
+public class ContainerSoulSmelter extends Container
+{
+
+	private final int PLAYER_INVENTORY_ROWS = 3;
+	private final int PLAYER_INVENTORY_COLUMNS = 9;
+
+	// 130, 54
+
+	public ContainerSoulSmelter(InventoryPlayer player, TileSoulSmelter soulSmelter)
+	{
+		this.addSlotToContainer(new Slot(soulSmelter, 0, 32, 22));
+
+		// Add the player's inventory slots to the container
+		for (int inventoryRowIndex = 0; inventoryRowIndex < PLAYER_INVENTORY_ROWS; ++inventoryRowIndex)
+		{
+			for (int inventoryColumnIndex = 0; inventoryColumnIndex < PLAYER_INVENTORY_COLUMNS; ++inventoryColumnIndex)
+			{
+				this.addSlotToContainer(new Slot(player, inventoryColumnIndex + inventoryRowIndex * 9 + 9, 9 + inventoryColumnIndex * 18, 60 + inventoryRowIndex * 18));
+			}
+		}
+
+		// Add the player's action bar slots to the container
+		for (int actionBarSlotIndex = 0; actionBarSlotIndex < PLAYER_INVENTORY_COLUMNS; ++actionBarSlotIndex)
+		{
+			this.addSlotToContainer(new Slot(player, actionBarSlotIndex, 9 + actionBarSlotIndex * 18, 119));
+		}
+	}
+
+	@Override
+	public boolean canInteractWith(EntityPlayer entityplayer)
+	{
+		return true;
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotIndex)
+	{
+		ItemStack itemStack = null;
+		Slot slot = (Slot) inventorySlots.get(slotIndex);
+		if (slot != null && slot.getHasStack())
+		{
+			ItemStack slotItemStack = slot.getStack();
+			itemStack = slotItemStack.copy();
+			if (slotIndex < 1)
+			{
+
+				if (!this.mergeItemStack(slotItemStack, 1, inventorySlots.size(), true))
+				{
+					return null;
+				}
+			}
+			else
+			{
+				if (!this.mergeItemStack(slotItemStack, 0, 1, false))
+				{
+					return null;
+				}
+			}
+
+			if (slotItemStack.stackSize == 0)
+			{
+				slot.putStack((ItemStack) null);
+			}
+			else
+			{
+				slot.onSlotChanged();
+			}
+		}
+		return itemStack;
+	}
+
+}
